26c72f8275253d23e90edcb21272237f5cacce48
LPatton4 <77860909+LPatton4@users.noreply.github.com>
2021-10-19 12:02:45 -0500

    changed css reset code and added js function for hasjs and nojs classes

--- js/site.js
+++ js/site.js
@@ -1,3 +1,9 @@
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+
 //Alert for minimum password length from Stack Overflow
 $('button[name="submit"]').click(function() {
     if($('input[name="password"]').val().length < 8) {
